3bb6f7ed5f38f9ca74f72cc8dadbc8ede9c98256
Suhyeon <suhyeon.b01@gmail.com>
2022-04-11 10:04:22 +0900

    initial commit

---
+++ streaming-site-clone/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MovieListSliderComponent } from './components/movie-list-slider/movie-list-slider.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieListSliderComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ streaming-site-clone/src/app/components/movie-list-slider/movie-list-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-slider',
+  templateUrl: './movie-list-slider.component.html',
+  styleUrls: ['./movie-list-slider.component.scss']
+})
+export class MovieListSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
